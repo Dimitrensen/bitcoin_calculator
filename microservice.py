1dec48b6844e37905b563ab22635a465c6521eea
dimitrensen <mitsen@hotmail.gr>
2024-07-18 15:23:12 +0200

    first libraries installed

--- microservice.py
+++ microservice.py
@@ -1,5 +1,14 @@
 from flask import Flask, jsonify, request
+import requests
 import json
 
+app = Flask(__name__)
+
+DATA_FILE = 'data.txt'
 # CoinGecko API URL
 API_URL = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur,czk'
+
+
+@app.route("/")
+def get_results():
+    return
